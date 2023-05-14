6ef0ecaec97d479670ed212a4ddd4045989b45be
rajat-soni-vst-au4 <rajatsoni.rs66@gmail.com>
2023-05-14 23:12:21 +0530

    Data print on UI from API

--- index.js
+++ index.js
@@ -29,3 +29,25 @@ function add(e){
    showStock()
 }
 
+//function to diaplay the data from API
+function showStock(){
+   try {
+      axios.get(url)
+      .then(array => {
+         //loop on the data's length (array has all the data of API)
+         for(let i=0; i<array.data.length; i++){
+            let newElem = document.createElement("li");
+            newElem.textContent = array.data[i].name +"   -   "+ array.data[i].desc + "   -   "+ array.data[i].price + "   -   "+ array.data[i].quant;
+            
+    
+            newElem.append(button1, button2, button3);
+            content.appendChild(newElem);
+          }
+        })
+      
+   } catch (error) {
+            console.log(error)
+   }
+
+}
+showStock()
